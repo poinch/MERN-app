69686319fe4ed5885989ffeee2a42391302c890f
Poinch <ponch92@hotmail.it>
2021-11-03 11:44:57 +0100

    Built Nav and Sidedrawer, CSSTransGroup installed, Portals used

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,11 +1,14 @@
 import UserItem from './UserItem'
+import Card from '../../shared/components/UIElements/Card'
 import './UsersList.css'
 
 const UsersList = ({ items }) => {
   if (items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found</h2>
+        <Card>
+          <h2>No users found</h2>
+        </Card>
       </div>
     )
   }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,12 +1,15 @@
 import UsersList from '../components/UsersList'
 
 const Users = () => {
-  const USERS = [{
-    id: 'ui1',
-    name: 'Mark Pollo',
-    image: 'https://images.pexels.com/photos/3417403/pexels-photo-3417403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    places: 3
-  }]
+  const USERS = [
+    {
+      id: 'ui1',
+      name: 'Mark Pollo',
+      image:
+        'https://images.pexels.com/photos/3417403/pexels-photo-3417403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3,
+    },
+  ]
 
   return <UsersList items={USERS} />
 }
